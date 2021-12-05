d139d369594733c45a3d1d0af074dfe8ee867c18
Ian Simpson <ian.simpson.nz@gmail.com>
2021-12-05 20:33:35 +1300

    Day 5 part 2

--- day05/vents/src/main.rs
+++ day05/vents/src/main.rs
@@ -1,35 +1,23 @@
 use std::fs;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Point {
     pub x: i32,
     pub y: i32,
 }
 
-impl Point {
-    pub fn print(&self) {
-        print!("({},{})", self.x, self.y);
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Line {
     pub start: Point,
     pub end: Point,
 }
 
 impl Line {
-    pub fn print(&self) {
-        self.start.print();
-        print!(" to ");
-        self.end.print();
-    }
-
     pub fn get_points(&self) -> Vec<Point> {
         let mut result:Vec<Point> = Vec::new();
 
-        if self.start.x == self.end.x {
+        if self.start.x == self.end.x {    // Vertical line
             let mut start = self.start.y;
             let mut end = self.end.y;
             if start > end {
@@ -40,7 +28,7 @@ impl Line {
             for y in start..=end {
                 result.push( Point { x: self.start.x, y: y } );
             }
-        } else if self.start.y == self.end.y {
+        } else if self.start.y == self.end.y {   // Horizontal line
             let mut start = self.start.x;
             let mut end = self.end.x;
             if start > end {
@@ -51,6 +39,30 @@ impl Line {
             for x in start..=end {
                 result.push( Point { x: x, y: self.start.y } );
             }
+        } else if (self.start.x - self.end.x).abs() == (self.start.y - self.end.y).abs() {  // Diagonal line
+            let mut x = self.start.x;
+            let mut y = self.start.y;
+            let mut x_delta = 1;
+            let mut y_delta = 1;
+
+            if self.start.x > self.end.x {
+                x_delta = -1;
+            }
+
+            if self.start.y > self.end.y {
+                y_delta = -1;
+            }
+
+            loop {
+                result.push( Point { x: x, y: y } );
+                if x == self.end.x {
+                    break;
+                }
+
+                x += x_delta;
+                y += y_delta;
+            }
+
         }
 
         result
@@ -77,13 +89,7 @@ fn main() {
             })
         .collect();
 
-/*
-    for l in lines {
-        l.print();
-        print!("  {:?}", l.get_points());
-        println!();
-    }
-*/
+
     let mut map:HashMap<Point, u32> = HashMap::new();
 
     for line in lines {
@@ -91,15 +97,10 @@ fn main() {
             if let Some(n) = map.get_mut(&point) {
                 *n += 1;
             } else {
-//                print!("Inserting ");
-//                point.print();
-//                println!();
                 map.insert(point, 1);
             }
         }
     }
-
-//    println!("Values = {:?}", map.values());
 
     println!("Answer is {}",
         map.values()
